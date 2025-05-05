291c0874c3e5674e34bb938c3f1c2f900551bb9d
doanxuandong <doanxuandong8303@gmail.com>
2025-05-05 15:03:55 +0700

    15pm_5/5/2025

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
 const panels = document.querySelectorAll('.panel')
 
+let currentIndex = 0;
+
+setInterval(() => {
+    removeActiveClasses();
+    panels[currentIndex].classList.add('active');
+    currentIndex = (currentIndex + 1) % panels.length;
+}, 3000);
+
 panels.forEach(panel => {
     panel.addEventListener('click', () => {
-        removeActiveClasses()
-        panel.classList.add('active')
-    })
-})
+        removeActiveClasses();
+        panel.classList.add('active');
+        currentIndex = Array.from(panels).indexOf(panel);
+    });
+});
 
 function removeActiveClasses() {
     panels.forEach(panel => {
-        panel.classList.remove('active')
-    })
+        panel.classList.remove('active');
+    });
 }
